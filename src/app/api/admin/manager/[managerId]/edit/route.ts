70f175d6f14d3aa481a6f364e34bc102d36d8b85
Pranay Nandiwadekar <142014316+pranayndv@users.noreply.github.com>
2025-12-29 12:09:48 +0530

    Refactor PATCH handler to use NextRequest

--- src/app/api/admin/manager/[managerId]/edit/route.ts
+++ src/app/api/admin/manager/[managerId]/edit/route.ts
@@ -1,16 +1,18 @@
-// src/app/api/managers/[managerId]/edit/route.ts
+
 import prisma from "@/lib/db/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth/auth";
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { RoleName } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
 export async function PATCH(
-  req: Request,
-  { params }: { params: { managerId: string } }
+  req: NextRequest,
+  context: { params: Promise<{ managerId: string }> }
 ) {
   try {
+    const { managerId } = await context.params;
+
     const session = await getServerSession(authOptions);
 
     if (!session?.user || session.user.role !== RoleName.ADMIN) {
@@ -20,14 +22,14 @@ export async function PATCH(
       );
     }
 
-    const { managerId } =await params;
     const { name, email, password } = await req.json();
 
-    const data : {name:string, email:string, password:string} = {
-        name: "",
-        email: "",
-        password: ""
-    };
+    const data: {
+      name?: string;
+      email?: string;
+      password?: string;
+    } = {};
+
     if (name) data.name = name;
     if (email) data.email = email;
     if (password) data.password = await bcrypt.hash(password, 10);
